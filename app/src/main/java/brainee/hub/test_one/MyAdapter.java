01020d98a1ad0ffe3e0b5462bdf6887a8213fc3c
Kimkims1 <kimanthikevin76@gmail.com>
2020-08-12 15:26:10 +0300

    viewPager - android

--- app/src/main/java/brainee/hub/test_one/MyAdapter.java
+++ app/src/main/java/brainee/hub/test_one/MyAdapter.java
@@ -1,7 +1,12 @@
 package brainee.hub.test_one;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
@@ -26,6 +31,48 @@ class MyAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        //inflate layout for the cards
+
+        View view = LayoutInflater.from(context).inflate(R.layout.card_item, container, false);
+
+        //Initialise views
+        ImageView imageViewIv = view.findViewById(R.id.imageView);
+        TextView titleTv = view.findViewById(R.id.title);
+        TextView descTv = view.findViewById(R.id.desc);
+        TextView dateTv = view.findViewById(R.id.date);
+
+        //get data
+        MyModel model = arrayList.get(position);
+
+        final String title = model.getTitle();
+        final String desc = model.getDesc();
+        final String date = model.getDate();
+        int image = model.getImage();
+
+        //Set data to respective uis
+        titleTv.setText(title);
+        descTv.setText(desc);
+        dateTv.setText(date);
+        imageViewIv.setImageResource(image);
+
+        //Handle card clicks
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, title + "\n" + desc + "\n" + date, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Add view to container
+        container.addView(view,position);
+
+        return view;
     }
 }
