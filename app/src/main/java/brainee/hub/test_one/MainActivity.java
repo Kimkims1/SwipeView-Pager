346574fb8833201f98d7d57b33706839a225f29d
Kimkims1 <kimanthikevin76@gmail.com>
2020-08-12 14:50:29 +0300

    viewPager - android

---
+++ app/src/main/java/brainee/hub/test_one/MainActivity.java
@@ -0,0 +1,14 @@
+package brainee.hub.test_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
